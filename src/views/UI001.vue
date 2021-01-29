b8de7ff0822e23ca3e5f26a74fae10fe26d25ec0
Kaarel Kallas <kaarel.kallas@ametikool.ee>
2021-01-29 09:59:47 +0200

    inputs

--- src/views/UI001.vue
+++ src/views/UI001.vue
@@ -4,5 +4,11 @@
       <h1>Siia tuleb esimene ülesanne (sign-up form)</h1>
       <p>Challenge: Design Hint... Create a sign up page, modal, form, or app screen related to signing up for something. It could be for a volunteer event, contest registration, a giveaway, or anything you can image.</p>
     </div>
+    <div>
+        <input/>
+        <label> Password</label>
+        <input/>
+        <button> Submit </button>
+    </div>
   </div>
 </template>
