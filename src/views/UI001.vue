6669c1ac1b35052361c20c81c2f3f6d5d09a2ada
matveimug <madis.vaher@gmail.com>
2021-02-09 15:55:21 +0200

    lil cooler

--- src/views/UI001.vue
+++ src/views/UI001.vue
@@ -2,6 +2,8 @@
   <div class="home-content">
     <div class="card">
       <h1>Sign up</h1>
+      <p>Create your user here by providing your e-mail and a password</p>
+      <hr>
       <form action="https://ametikool.ee">
         <div class="form-group">
           <label for="email">E-mail</label>
@@ -25,21 +27,42 @@
 * {
   box-sizing: border-box;
 }
+html, body {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.nav a {
+  color: #fff;
+  text-decoration: none;
+}
 .home-content {
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   text-align: left;
   padding: 5em;
+  height: 100%;
 }
 h1 {
-  margin: 0 0 1em 0;
+  margin: 0 0 0.5em 0;
+}
+p {
+  line-height: 1.4;
+  margin-bottom: 1em;
+}
+hr {
+  border: 0;
+  border-bottom: 1px solid #eee;
+  margin-bottom: 1rem;
 }
 .card {
   width: 400px;
   padding: 3em 2em;
-  box-shadow: 0 0 40px rgba(0,0,0,0.3);
+  box-shadow: 0 0 40px rgba(0, 0, 0, 0.3);
   border-radius: 10px;
+  background-color: #fff;
 }
 .form-group {
   margin-bottom: 1em;
@@ -47,7 +70,9 @@ h1 {
 .form-group input {
   width: 100%;
   display: block;
-  padding: 0.5em;
+  padding: 1em;
+  border-radius: 5px;
+  border: 1px solid rgb(207, 207, 207);
 }
 .form-group label {
   display: block;
@@ -56,6 +81,15 @@ h1 {
 }
 .button {
   width: 100%;
-  padding: 0.5em;
+  padding: 1em;
+  margin-top: 1em;
+  background: linear-gradient(60deg, rgb(0, 102, 255), rgb(0, 153, 255));
+  border: none;
+  color: #fff;
+  font-weight: bold;
+  border-radius: 5px;
+}
+body {
+  background: linear-gradient(60deg, rgb(0, 102, 255), rgb(0, 153, 255));
 }
 </style>
