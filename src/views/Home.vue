4db2e4022342e2cf12717f59a93fcb743979652d
kaurluhaaar <kaur.luhaaar@ametikool.ee>
2021-02-19 13:51:54 +0200

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,4 +4,5 @@
     Siin on siuke leht/web-app/asi, kuhu tulevad teie Madise tunni UI ülesanded. Esimene ül
     tuleb faili <code>views/UI001</code>, teine <code>views/UI002</code> jne. 
   </p>
+  
 </template>
--- src/views/UI002.vue
+++ src/views/UI002.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="flex-center">
     <div class="home-content">
-      <h1>Siia tuleb teine ülesanne (omal valikul)</h1>
-      <p>Kogu list - https://gist.github.com/Pustur/3b2bee790d2c8a966136d76a9157d5e4</p>
+      <img src="404code.png">
     </div>
   </div>
 </template>
+<style>
+  img {
+    object-fit: contain;
+    height: 720px;
+  }
+</style>
