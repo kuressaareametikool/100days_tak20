96d6cf82d15dce44870f674d2639f1522fc8e4ee
erik <erik.leemet@ametikool.ee>
2021-03-11 10:45:01 +0200

    first commit

--- src/views/UI003.vue
+++ src/views/UI003.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="flex-center">
     <div class="home-content">
-      .d
-      <h1>Get free Iphone</h1>
+      <h1>Get free Iphone<br>just enter you E-mail and get<br>1 Free Iphone</h1>
+        <div class="form-group">
+          <label for="email">Enter E-mail here</label>
+          <input id="email" type="email" />
+        </div>
+      <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-family-hero?wid=940&amp;hei=1112&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1604021663000" alt="Iphone">
     </div>
   </div>
+</template>
   <style>
-
+    .form-group {
+  margin-bottom: 1em;
+}
+.form-group input {
+  width: 100%;
+  display: block;
+  padding: 1em;
+  border-radius: 5px;
+  border: 1px solid rgb(207, 207, 207);
+}
+.form-group label {
+  display: block;
+  margin-bottom: 0.5em;
+  font-size: 0.8em;
+}
   </style>
