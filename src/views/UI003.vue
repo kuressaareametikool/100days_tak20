8aab6120fc8a04025a55aaf76110535df87bb915
macuway <mynameismgamer@gmail.com>
2021-03-18 12:53:56 +0200

    kass

--- src/views/UI003.vue
+++ src/views/UI003.vue
@@ -5,22 +5,22 @@
       <p>Songs: (plz no copyright, this is for a school project I swear!)</p>
       <p>The General Lee - Johnny Cash</p>
       <audio controls="controls">
-        <source src="/src/assets/General Lee.mp3" type="audio/mpeg" />
+        <source src="../assets/General Lee.mp3" type="audio/mpeg" />
       Your browser is trash and does not support the audio player... How sad.
       </audio>
        <p>Mantra - Kaz Moon</p>
       <audio controls="controls">
-        <source src="/src/assets/mantra.mp3" type="audio/mpeg" />
+        <source src="../assets/mantra.mp3" type="audio/mpeg" />
       Your browser is trash and does not support the audio player... How sad.
       </audio>
        <p>Do I Wanna Know - Arctic Monkeys</p>
       <audio controls="controls">
-        <source src="/src/assets/am-diwk.mp3" type="audio/mpeg" />
+        <source src="../assets/am-diwk.mp3" type="audio/mpeg" />
       Your browser is trash and does not support the audio player... How sad.
       </audio>
        <p>Again - Wun Two</p>
       <audio controls="controls">
-        <source src="/src/assets/Again.mp3" type="audio/mpeg" />
+        <source src="../assets/Again.mp3" type="audio/mpeg" />
       Your browser is trash and does not support the audio player... How sad.
       </audio>
     </div>
