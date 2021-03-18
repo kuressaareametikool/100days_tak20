96246dd4f4f023a08c0910d35e72b4aee16b7bf2
macuway <mynameismgamer@gmail.com>
2021-03-18 13:06:54 +0200

    Fixed music player

--- src/views/UI003.vue
+++ src/views/UI003.vue
@@ -5,22 +5,22 @@
       <p>Songs: (plz no copyright, this is for a school project I swear!)</p>
       <p>The General Lee - Johnny Cash</p>
       <audio controls="controls">
-        <source src="../assets/General Lee.mp3" type="audio/mpeg" />
+        <source src="/src/assets/1.mp3" type="audio/mpeg" />
       Your browser is trash and does not support the audio player... How sad.
       </audio>
        <p>Mantra - Kaz Moon</p>
       <audio controls="controls">
-        <source src="../assets/mantra.mp3" type="audio/mpeg" />
+        <source src="/src/assets/2.mp3" type="audio/mpeg" />
       Your browser is trash and does not support the audio player... How sad.
       </audio>
        <p>Do I Wanna Know - Arctic Monkeys</p>
       <audio controls="controls">
-        <source src="../assets/am-diwk.mp3" type="audio/mpeg" />
+        <source src="/src/assets/3.mp3" type="audio/mpeg" />
       Your browser is trash and does not support the audio player... How sad.
       </audio>
        <p>Again - Wun Two</p>
       <audio controls="controls">
-        <source src="../assets/Again.mp3" type="audio/mpeg" />
+        <source src="/src/assets/4.mp3" type="audio/mpeg" />
       Your browser is trash and does not support the audio player... How sad.
       </audio>
     </div>
